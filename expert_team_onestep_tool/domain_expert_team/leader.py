faaa1102acb2c6a5afd80b9b41d4ce0e045f7fb2
LB-Young <lby15356@gmail.com>
2024-11-12 16:49:46 +0800

    expert team one step tool

--- expert_team_onestep_tool/domain_expert_team/leader.py
+++ expert_team_onestep_tool/domain_expert_team/leader.py
@@ -1,4 +1,6 @@
 import re
+import json
+from domain_expert_team.code_execute import code_execute
 
 class Leader:
     def __init__(self, role_leader, agents, client, model):
@@ -6,20 +8,50 @@ class Leader:
         self.agents = agents
         self.llm_client = client
         self.model = model
+        self.tools = {"code_execute": code_execute}
 
     async def agent_run(self, agent_name, agent_job):
-
         pass
 
+    async def tool_run(self, tool_message):
+        function_name, function_params = tool_message.split(":", 1)
+        function_params_json = json.loads(function_params)
+        need_params = await self.tools[function_name](params_format=True)
+        extract_params = {}
+        for param in need_params:
+            extract_params[param] = function_params_json[param]
+        result = await self.tools[function_name](**extract_params)
+        return result
+
     async def execute(self, qeury):
-        prompt = self.role.format(prompt=qeury).strip()
+        prompt = self.role.replace("{prompt}", qeury).strip()
         messages = [{"role": "user", "content": prompt}]
         result = self.llm_client.chat.completions.create(
                 model=self.model,  # 请填写您要调用的模型名称
                 messages=messages,
-                stream=True,
+                stream=True
             )
+        all_answer = ""
+        tool_messages = ""
+        tool_Flag = False
         for chunk in result:
+            all_answer += chunk.choices[0].delta.content
+            if tool_Flag:
+                tool_messages += chunk.choices[0].delta.content
+                continue
+            if ":" in chunk.choices[0].delta.content and "=>$" in all_answer:
+                tool_Flag = True
+                tool_messages += chunk.choices[0].delta.content
+                yield ": "
+                continue
             yield chunk.choices[0].delta.content
+        if tool_Flag:
+            tool_messages = all_answer.split("=>$")[-1]
+            result = await self.tool_run(tool_message=tool_messages)
+            for item in str(result+"\n"):
+                yield item
+            query = qeury + "\n" + "已经执行内容:" + all_answer + "\n" + "工具执行结果:" + result
+            async for item in self.execute(qeury=query):
+                yield item
         # result = result.choices[0].message.content
         # print("agent_result:", result)
--- expert_team_onestep_tool/main.py
+++ expert_team_onestep_tool/main.py
@@ -3,12 +3,14 @@ from domain_expert_team.leader import Leader
 from llm_client import client, model
 
 async def main():
-    with open(r"F:\Cmodels\Zero_Agent\expert_team_onestep\job_describe.txt", "r", encoding="utf-8") as f:
+    with open(r"F:\Cmodels\Zero_Agent\expert_team_onestep_tool\job_describe.txt", "r", encoding="utf-8") as f:
         job_describe = f.read()
     leader = Leader(role_leader=job_describe, agents=None, client=client, model=model)
     # result = await leader.execute("我是高考生，现在想要选专业，但是不知道选什么专业。请你介绍一下金融、法律和计算机三个专业分别有什么优点和缺点。")
     # query = "我是高考生，现在想要选专业，但是不知道选什么专业。请你介绍一下金融、法律和计算机三个专业分别有什么优点和缺点。"
-    query = "炒股的时候什么时候买进比较好？"
+    # query = "炒股的时候什么时候买进比较好？"
+    # query = "请帮我生成一段冒泡排序代码和一段归并排序代码，调用代码执行器运行生成的代码，基于测试结果对比一下两种排序算法的优劣。"
+    query = "请帮我生成一段选择排序的代码，调用代码执行器运行生成的代码，基于结果分析一下选择排序的特点"
     async for item in leader.execute(qeury=query):
         print(item, end="", flush=True)
 
